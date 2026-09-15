c05337bcc92f23703c3b24aed2af5ec5c0331f71
AEntropia <sandman2871@gmail.com>
2026-09-15 19:40:03 -0300

    ajeitando

--- Source/Ascendencia/Ascendencia.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Ascendencia.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ascendencia, "Ascendencia" );
-
-DEFINE_LOG_CATEGORY(LogAscendencia)
- 
--- Source/Ascendencia/AscendenciaGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AscendenciaGameMode.h"
-
-AAscendenciaGameMode::AAscendenciaGameMode()
-{
-	// stub
-}
